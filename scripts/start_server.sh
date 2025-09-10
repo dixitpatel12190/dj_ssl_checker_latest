bb5522c2914cfd5b49ca476732b647d22db39856
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 23:29:37 +0530

    Added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker pull dixitpatel12190/dj_ssl_checker_latest
-docker run -d -p 80:8000 dixitpatel12190/dj_ssl_checker_latest
+docker run -d -p 8000:8000 dixitpatel12190/dj_ssl_checker_latest
 
 sudo systemctl restart gunicorn
 sudo systemctl enable gunicorn
