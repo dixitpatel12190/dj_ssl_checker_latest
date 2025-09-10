166dc82ce4f95d3d4d71741ab4b9640356fc43a6
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 21:57:45 +0530

    Added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -15,14 +15,19 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
     gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo usermod -aG docker $USER
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
   https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+if [ "$USER" != "root" ]; then
+    sudo usermod -aG docker "$USER"
+    echo "Added $USER to docker group.
+fi
+
+
 
