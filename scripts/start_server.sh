ca6ed4fb63c189696dabbb53feeaaa8f1ea7c831
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 23:55:24 +0530

    Added new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,8 +4,6 @@ set -e
 docker pull dixitpatel12190/dj_ssl_checker_latest
 docker run -d -p 8000:8000 dixitpatel12190/dj_ssl_checker_latest
 
-sudo cp scripts/geetagyan.org.conf /etc/nginx/sites-available/geetagyan.org
-
 sudo ln -sf /etc/nginx/sites-available/geetagyan.org /etc/nginx/sites-enabled/
 
 echo "Nginx configuration updated."
