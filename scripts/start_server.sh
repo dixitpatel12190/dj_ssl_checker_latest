6298b8cfe87ba45549d84e5f3e52e3ec389f4f2d
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 22:46:15 +0530

    Added new

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,20 +7,7 @@ docker run -d -p 80:8000 dixitpatel12190/dj_ssl_checker_latest
 sudo systemctl restart gunicorn
 sudo systemctl enable gunicorn
 
-sudo tee /etc/nginx/sites-available/geetagyan.org > /dev/null <<'EOF'
-server {
-    listen 80;
-    server_name geetagyan.org;
-
-    location / {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_http_version 1.1;
-        proxy_set_header Connection "";
-
-        proxy_set_header Host geetagyan.org;
-    }
-}
-EOF
+sudo cp geetagyan.org.conf /etc/nginx/sites-available/geetagyan.org
 
 sudo ln -sf /etc/nginx/sites-available/geetagyan.org /etc/nginx/sites-enabled/
 
