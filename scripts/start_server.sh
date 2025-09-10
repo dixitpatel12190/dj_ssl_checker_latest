d7904abd785dccef5a4d296604407975193b4793
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 23:37:27 +0530

    Added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,6 @@ set -e
 docker pull dixitpatel12190/dj_ssl_checker_latest
 docker run -d -p 8000:8000 dixitpatel12190/dj_ssl_checker_latest
 
-sudo systemctl restart gunicorn
-sudo systemctl enable gunicorn
-
 sudo cp geetagyan.org.conf /etc/nginx/sites-available/geetagyan.org
 
 sudo ln -sf /etc/nginx/sites-available/geetagyan.org /etc/nginx/sites-enabled/
