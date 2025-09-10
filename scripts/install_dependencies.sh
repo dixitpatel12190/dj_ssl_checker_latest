a3dd889b282e7012cb0dd79be765b2a7d03bd62a
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-10 21:31:43 +0530

    Added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+set -e
+
 sudo apt-get update
 sudo apt-get install -y nginx
-sudo systemctl enable nginx
 
-docker pull dixitpatel12190/dj_ssl_checker_latest
-docker run -d -p 80:8000 dixitpatel12190/dj_ssl_checker_latest
+sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+set -e
+
+docker pull dixitpatel12190/dj_ssl_checker_latest
+docker run -d -p 80:8000 dixitpatel12190/dj_ssl_checker_latest
+
 sudo systemctl restart gunicorn
 sudo systemctl enable gunicorn
-sudo systemctl start nginx
+
+sudo rm default
+sudo touch default
+
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+server {
+    listen 80;
+    server_name geetagyan.org;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Connection "";
+
+        proxy_set_header Host geetagyan.org;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOF
+
+sudo systemctl restart nginx
