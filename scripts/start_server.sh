5b1516aca4aa597527224cda2313f041a0ad78b6
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-11 11:21:36 +0530

    Added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,12 @@ set -e
 
 docker pull dixitpatel12190/dj_ssl_checker_latest
 
-sudo docker rm -f $(sudo docker ps -q --filter "publish=8000")
+CID=$(sudo docker ps -q --filter "publish=8000")
+if [ -n "$CID" ]; then
+  sudo docker rm -f $CID
+else
+  echo "No container found on port 8000"
+fi
 
 docker run -d -p 8000:8000 dixitpatel12190/dj_ssl_checker_latest
 
