e4e57b62a50530529f71ec54c5e5d0070e729730
Dixit Ranpariya <dixit.d.ranpariya@team.telstra.com>
2025-09-11 11:14:43 +0530

    Added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,9 @@
 set -e
 
 docker pull dixitpatel12190/dj_ssl_checker_latest
+
+sudo docker rm -f $(sudo docker ps -q --filter "publish=8000")
+
 docker run -d -p 8000:8000 dixitpatel12190/dj_ssl_checker_latest
 
 echo "Nginx configuration updated."
